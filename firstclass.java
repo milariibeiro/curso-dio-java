b67bd85d49a3ab5e45ee2c9ab5fbc86acfff547a
milariibeiro <104395797+milariibeiro@users.noreply.github.com>
2022-05-13 14:55:46 -0300

    Add files via upload

---
+++ firstclass.java
@@ -0,0 +1,24 @@
+package com.dio;
+
+import com.dio.model.project1;
+
+public class firstclass {
+	
+	 public static void main(String[] args) {
+		
+		 project1 project1 = new project1(); 
+		livros livros = new livros;
+		
+		System.out.println(project1);
+		System.out.println(livros);
+		 /*int a = 2;
+		 int b = 3;
+		 int c = -10;
+		 System.out.println(" hello world! " + (a+b+c));*/
+	 }
+}
+
+class Livros {
+	private String nome;
+	private String npag;
+}
