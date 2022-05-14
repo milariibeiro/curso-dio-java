449730369b4371dba0ce000f2d66837c891d2f14
milariibeiro <104395797+milariibeiro@users.noreply.github.com>
2022-05-14 01:18:29 -0300

    Update main.class3.java

--- main.class3.java
+++ main.class3.java
@@ -1,10 +1,5 @@
-package one.digitalinnovation.basecamp;
 
-/**
- * Classe de exemplo para o exercício da Aula 3 de Variáveis, Tipos de Dado e Operadores aritméticos.
- */
-
-public class Main3 {
+public class main.class3 {
 
     public static void main(String[] args) {
 
