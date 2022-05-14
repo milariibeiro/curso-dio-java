de7197b5761f8acd2bb990fb7ca50bd525bdb8ad
milariibeiro <104395797+milariibeiro@users.noreply.github.com>
2022-05-14 01:16:28 -0300

    Update CalculadoraMet.java

--- CalculadoraMet.java
+++ CalculadoraMet.java
@@ -1,34 +1,32 @@
-/**
- * Classe de exemplo para o exercício da Aula 1 de Métodos.
- */
-public class Calculadora {
+
+public class CalculadoraMet {
 
     public static void soma(double numero1, double numero2) {
 
         double resultado = numero1 + numero2;
 
-        System.out.println("A soma de " + numero1 + " mais " + numero2 + " é " + resultado);
+        System.out.println("A soma de " + numero1 + " mais " + numero2 + " Ã© " + resultado);
     }
 
     public static void subtracao(double numero1, double numero2) {
 
         double resultado = numero1 - numero2;
 
-        System.out.println("A subtracao de " + numero1 + " menos " + numero2 + " é " + resultado);
+        System.out.println("A subtracao de " + numero1 + " menos " + numero2 + " Ã© " + resultado);
     }
 
     public static void multiplicacao(double numero1, double numero2) {
 
         double resultado = numero1 * numero2;
 
-        System.out.println("A multiplicação de " + numero1 + " vezes " + numero2 + " é " + resultado);
+        System.out.println("A multiplicaÃ§Ã£o de " + numero1 + " vezes " + numero2 + " Ã© " + resultado);
     }
 
     public static void  divisao(double numero1, double numero2) {
 
         double resultado = numero1 / numero2;
 
-        System.out.println("A divisão de " + numero1 + " por " + numero2 + " é " + resultado);
+        System.out.println("A divisÃ£o de " + numero1 + " por " + numero2 + " Ã© " + resultado);
     }
 
 }
